32142567957039e86d47c632c6d7889bb1473fd6
Alexander Gerstberger <a.gerstberger@web.de>
2023-05-20 15:45:01 +0200

    adding IncomeCard component

---
+++ src/components/income.tsx
@@ -0,0 +1,15 @@
+import { Income} from "@prisma/client";
+
+type IncomeProps = {
+    income: Income;
+}
+
+export default function IncomeCard({income}: IncomeProps) {
+    return (
+        <div className="income-card">
+            <h2 className="income-card__title">{income.name}</h2>
+            <p className="income-card__amount">{income.amount}</p>
+            <p className="income-card__date">{income.date.toLocaleDateString()}</p>
+        </div>
+    )
+}
