aea8de1e4a6dc76f7900ba71bbaf460badece883
Alexander Gerstberger <a.gerstberger@web.de>
2023-05-21 14:40:46 +0200

    creating the /add route

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
+
 export default function Add() {
     return (
-        <div>
-            <h1>Add</h1>
+        <div style={{
+            display: "contents",
+            textAlign: "center",
+        }}>
+            <h1>Create new</h1>
+            <Link href="/incomes/new" className="add-option">Income</Link>
+            <Link href="/expenses/new" className="add-option">Expense</Link>
         </div>
     )
 }
