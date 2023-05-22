854bf9f7c4d5acc50188934aa2971cb271e4908d
Alexander Gerstberger <a.gerstberger@web.de>
2023-05-22 11:45:34 +0200

    adding /incomes

--- src/app/incomes/page.tsx
+++ src/app/incomes/page.tsx
@@ -1,5 +1,19 @@
-export default function Incomes() {
-  return (
-      <p>Income page</p>
-  )
+import prisma from "@/db";
+import IncomeCard from "@/components/income";
+
+export const dynamic = "force-dynamic";
+
+export default async function Incomes() {
+    const incomes = await prisma.income.findMany();
+
+    return (
+        <>
+            <h1>Incomes</h1>
+            {
+                incomes.map(income => {
+                    return IncomeCard({income})
+                })
+            }
+        </>
+    )
 }
