57a9303a27f03f660e02c22920fdc096ce0f9623
Alexander Gerstberger <a.gerstberger@web.de>
2023-05-30 21:19:45 +0200

    adding new income page

---
+++ src/app/incomes/new/incomeForm.tsx
@@ -0,0 +1,34 @@
+import {saveIncome} from "@/app/incomes/new/saveIncome";
+import MultiSelect from "@/components/multi-select";
+import prisma from "@/db";
+
+export function IncomeForm() {
+    const searchExpenses = async (search: string): Promise<{[key: string]: string}> => {
+        "use server";
+        const expenses = await prisma.expense.findMany({
+            where: {
+                name: {
+                    contains: search
+                }
+            }
+        });
+
+        return expenses.reduce((map, expense) => {
+            map[expense.id] = expense.name;
+            return map;
+        }, {} as { [key: string]: string });
+    }
+    return (
+        <form action={saveIncome}>
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" name="name"/>
+            <label htmlFor="amount">Amount</label>
+            <input type="number" id="amount" name="amount"/>
+            <label htmlFor="date">Date</label>
+            <input type="date" id="date" name="date"/>
+            <label htmlFor="expenses">Expenses</label>
+            <MultiSelect name={"expenses"} updateOptions={searchExpenses}/>
+            <button type="submit" style={{width: "auto"}}>Add Income</button>
+        </form>
+    )
+}
---
+++ src/app/incomes/new/saveIncome.tsx
@@ -0,0 +1,26 @@
+"use server";
+
+import prisma from "@/db";
+import {redirect} from "next/navigation";
+
+export async function saveIncome(formData: FormData) {
+    const name = formData.get("name");
+    const amount = formData.get("amount");
+    const date = formData.get("date");
+    const expenses: {[key: string]: string} = JSON.parse(formData.get("expenses") as string);
+
+    const expensesIds = Object.keys(expenses).map(expenseId => ({id: Number(expenseId)}));
+
+    await prisma.income.create({
+        data: {
+            name: name as string,
+            amount: Number(amount),
+            date: new Date(date as string),
+            expenses: {
+                connect: expensesIds
+            }
+        }
+    });
+
+    redirect("/incomes")
+}
