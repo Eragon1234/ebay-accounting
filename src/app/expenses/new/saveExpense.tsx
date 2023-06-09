f4faafc595502fff104608c0b84e4125a733e2e5
eragon <a.gerstberger@web.de>
2023-06-09 21:59:36 +0200

    adding form data validation to saveExpense

--- src/app/expenses/new/saveExpense.tsx
+++ src/app/expenses/new/saveExpense.tsx
@@ -1,24 +1,63 @@
 "use server";
 
 import prisma from "@/db";
-import {ExpenseType} from "@prisma/client";
+import {ExpenseType, Prisma} from "@prisma/client";
 import {revalidatePath} from "next/cache";
 import {redirect} from "next/navigation";
+import zod from "zod";
+
+const newExpenseSchema = zod.object({
+    name: zod.string({
+        required_error: "missing name",
+    }),
+    date: zod.date({
+        required_error: "missing date",
+        coerce: true
+    }),
+    amount: zod.number({
+        required_error: "missing amount",
+        coerce: true
+    }),
+    expenseType: zod.nativeEnum(ExpenseType, {
+        required_error: "missing expense type",
+    }),
+})
 
 export default async function saveExpense(formData: FormData) {
-    const name = formData.get("name");
-    const amount = formData.get("amount");
-    const type = formData.get("type");
-    const date = formData.get("date");
+    try {
+        const {
+            name,
+            date,
+            amount,
+            expenseType,
+        } = newExpenseSchema.parse({
+            name: formData.get("name"),
+            date: formData.get("date"),
+            amount: formData.get("amount"),
+            expenseType: formData.get("type"),
+        });
 
-    await prisma.expense.create({
-        data: {
-            name: name as string,
-            type: ExpenseType[type as keyof typeof ExpenseType],
-            amount: Number(amount),
-            date: new Date(date as string),
+        await prisma.expense.create({
+            data: {
+                name,
+                type: expenseType,
+                amount,
+                date,
+            }
+        });
+    } catch (e) {
+        if (e instanceof zod.ZodError) {
+            return {
+                error: e.message,
+            }
+        } else if (e instanceof Prisma.PrismaClientKnownRequestError) {
+            return {
+                error: e.message,
+            }
+        } else {
+            throw e;
         }
-    });
+    }
 
     revalidatePath("/expenses");
     redirect("/expenses");
