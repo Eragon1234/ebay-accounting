7dc4faf74adbe24a4bb0097991e52bc2a2ba4b4d
Alexander Gerstberger <a.gerstberger@web.de>
2023-09-23 20:05:00 +0200

    replacing the inter-font from Google fonts with Tahoma

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import './globals.css'
-import {Inter} from 'next/font/google'
 import Header from "@/app/header";
 import React from "react";
-
-const inter = Inter({subsets: ['latin']})
 
 export const metadata = {
     title: 'eBay Accounting',
@@ -13,7 +10,7 @@ export const metadata = {
 export default function RootLayout({children}: { children: React.ReactNode }) {
     return (
         <html lang="en">
-        <body className={inter.className}>
+        <body>
         <Header/>
         <main>{children}</main>
         </body>
