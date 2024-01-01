94fff70d4b81c9c712f374f3fb6ac2e8c2210bdf
Alexander Gerstberger <a.gerstberger@web.de>
2024-01-01 12:30:45 +0100

    Make the /add page not centered

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -3,18 +3,13 @@ import IncomeIcon from "@/components/icons/income-icon";
 import ExpenseIcon from "@/components/icons/expense-icon";
 
 export default function Add() {
-    return (
-        <div style={{
-            display: "contents",
-            textAlign: "center",
-        }}>
-            <h1>Create new</h1>
-            <Link href="/incomes/new" className="add-option">
-                <IncomeIcon/>Income
-            </Link>
-            <Link href="/expenses/new" className="add-option">
-                <ExpenseIcon/>Expense
-            </Link>
-        </div>
-    )
+    return <>
+        <h1>Create new</h1>
+        <Link href="/incomes/new" className="add-option">
+            <IncomeIcon/>Income
+        </Link>
+        <Link href="/expenses/new" className="add-option">
+            <ExpenseIcon/>Expense
+        </Link>
+    </>
 }
