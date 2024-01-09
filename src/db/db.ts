536c7d08837dc5c2c400ca1707de68cc5442f0cf
Alexander Gerstberger <a.gerstberger@web.de>
2024-01-09 21:47:01 +0100

    Turn off prisma query logging

--- src/db/db.ts
+++ src/db/db.ts
@@ -6,9 +6,7 @@ const globalForPrisma = global as unknown as {
 
 const prisma =
     globalForPrisma.prisma ??
-    new PrismaClient({
-        log: ['query'],
-    })
+    new PrismaClient()
 
 export default prisma
 
