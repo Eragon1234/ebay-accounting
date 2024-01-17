65b6f20e79ade52d1b9161932bd2f12e9678a228
Alexander Gerstberger <a.gerstberger@web.de>
2024-01-17 11:27:22 +0100

    Remove database migration from db.ts

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,14 +1,6 @@
 import {drizzle} from "drizzle-orm/node-postgres";
 import {Client} from "pg";
 import * as schema from "./schema";
-import {migrate} from "drizzle-orm/node-postgres/migrator";
-
-const migrationClient = new Client({
-    connectionString: process.env.DATABASE_URL,
-});
-migrate(drizzle(migrationClient), {
-    migrationsFolder: "../../drizzle"
-});
 
 const queryClient = new Client({
     connectionString: process.env.DATABASE_URL,
