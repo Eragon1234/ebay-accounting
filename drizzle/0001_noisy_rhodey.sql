4b36e14c17f79c8b19fb674c2f32296212ab9198
Alexander Gerstberger <a.gerstberger@web.de>
2024-01-18 20:49:46 +0100

    Remove unused things

--- drizzle/0001_noisy_rhodey.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "Expense" ALTER COLUMN "amount" SET DATA TYPE numeric;--> statement-breakpoint
-ALTER TABLE "Expense" ALTER COLUMN "date" SET DATA TYPE date;--> statement-breakpoint
-ALTER TABLE "Income" ALTER COLUMN "amount" SET DATA TYPE numeric;--> statement-breakpoint
-ALTER TABLE "Income" ALTER COLUMN "date" SET DATA TYPE date;
--- drizzle/0002_soft_kree.sql
+++
@@ -1,5 +0,0 @@
-UPDATE "Expense" SET amount = (amount * 1000000);
-UPDATE "Income" SET amount = (amount * 1000000);
-
-ALTER TABLE "Expense" ALTER COLUMN "amount" SET DATA TYPE bigint;--> statement-breakpoint
-ALTER TABLE "Income" ALTER COLUMN "amount" SET DATA TYPE bigint;
