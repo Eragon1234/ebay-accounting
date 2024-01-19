5fb99bf8acc526fdbb781f78e22105d2ca73d087
Alexander Gerstberger <a.gerstberger@web.de>
2024-01-19 16:47:56 +0100

    Fix the use of async/await in a client component

--- src/app/[lang]/expenses/new/expenseForm.tsx
+++ src/app/[lang]/expenses/new/expenseForm.tsx
@@ -1,16 +1,11 @@
-"use client";
-
 import createExpenseFromForm, {getExpenseTypes} from "@/db/expense";
-import {useState} from "react";
 import {getDictionary} from "@/translation/dictionaries";
 import {AsyncReturnType} from "@/types/asyncReturnType";
-import {TaxType} from "@/db/schema";
+import {TaxTypeInput} from "@/app/[lang]/expenses/new/taxTypeInput";
 
 export async function ExpenseForm({dict}: { dict: AsyncReturnType<typeof getDictionary> }) {
     const today = new Date();
     const isoDateString = today.toISOString().slice(0, 10);
-
-    const [taxType, setTaxType] = useState<string>(TaxType.VAT);
 
     const types = await getExpenseTypes();
 
@@ -26,15 +21,7 @@ export async function ExpenseForm({dict}: { dict: AsyncReturnType<typeof getDict
                 }
             </datalist>
             <label htmlFor="taxType">{dict.addExpense.taxType}</label>
-            <select id="taxType" name="taxType" value={taxType} onChange={e => setTaxType(e.target.value)}>
-                {
-                    Object.keys(TaxType).map((key) => (
-                        <option key={key}
-                                value={key}>{dict.addExpense.taxTypeEnum[key as keyof typeof TaxType]}</option>
-                    ))
-                }
-            </select>
-            {taxType === TaxType.VAT && <input type="number" id="vat" name="vat" required/>}
+            <TaxTypeInput dict={dict}/>
             <label htmlFor="amount">{dict.addExpense.amount}</label>
             <input type="number" id="amount" name="amount" step="0.01" required/>
             <label htmlFor="date">{dict.addExpense.date}</label>
---
+++ src/app/[lang]/expenses/new/taxTypeInput.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import {AsyncReturnType} from "@/types/asyncReturnType";
+import {getDictionary} from "@/translation/dictionaries";
+import {useState} from "react";
+import {TaxType} from "@/db/schema";
+
+export function TaxTypeInput({dict}: { dict: AsyncReturnType<typeof getDictionary> }) {
+
+    const [taxType, setTaxType] = useState<string>(TaxType.VAT);
+
+    return <>
+        <select id="taxType" name="taxType" value={taxType} onChange={e => setTaxType(e.target.value)}>
+            {
+                Object.keys(TaxType).map(key =>
+                    <option key={key}
+                            value={key}>{dict.addExpense.taxTypeEnum[key as keyof typeof TaxType]}</option>
+                )
+            }
+        </select>
+        {taxType === TaxType.VAT && <input type="number" id="vat" name="vat" required/>}
+    </>;
+}
