7866b182cc0abc528df514c5fc47df0bd9fe3e11
Alexander Gerstberger <a.gerstberger@web.de>
2024-02-10 21:20:37 +0100

    Remove the use of useFormStatus because of weird behavior

--- src/components/submit-button/submitButton.tsx
+++ src/components/submit-button/submitButton.tsx
@@ -1,9 +1,3 @@
-"use client";
-
-import {useFormStatus} from "react-dom";
-
 export async function SubmitButton({text}: { text: string }) {
-    const {pending} = useFormStatus();
-
-    return <button type="submit" style={{width: "auto",}} disabled={pending}>{text}</button>;
+    return <button type="submit" style={{width: "auto",}}>{text}</button>;
 }
