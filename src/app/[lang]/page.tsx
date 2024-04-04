069a144c51b98562a77c9cb7c8f9de54f2f6685f
Alexander Gerstberger <a.gerstberger@web.de>
2024-04-04 12:07:38 +0200

    Add range picker for dashboard cards

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,8 +1,11 @@
+"use client";
+
 import {getIncomeInRange} from "@/db/income";
 import {getExpenseInRange, getExpenseInRangeByType} from "@/db/expense";
 import {getDictionary, Locales} from "@/translation/dictionaries";
 import {euroToMicroEuro} from "@/db/schema";
 import {getVATInRange} from "@/db/tax";
+import {useEffect, useState} from "react";
 
 export const dynamic = "force-dynamic";
 
@@ -13,15 +16,45 @@ export default async function Home({params}: { params: { lang: Locales } }) {
     const yearBegin = new Date(Date.UTC(now.getFullYear(), 0));
     const yearEnd = new Date(Date.UTC(now.getFullYear(), 11, 31));
 
-    const income = await getIncomeInRange(yearBegin, yearEnd) / euroToMicroEuro;
-    const expense = await getExpenseInRange(yearBegin, yearEnd) / euroToMicroEuro;
-    const earnings = income - expense;
+    const [rangeStart, setRangeStart] = useState(yearBegin);
+    const [rangeEnd, setRangeEnd] = useState(yearEnd);
 
-    const vatToPay = await getVATInRange(yearBegin, yearEnd) / euroToMicroEuro;
+    const [income, setIncome] = useState(0);
+    const [earnings, setEarnings] = useState(0);
+    const [vatToPay, setVatToPay] = useState(0);
+    const [expenseByType, setExpenseByType] = useState<Record<string, number>>({});
 
-    const expenseByType = await getExpenseInRangeByType(yearBegin, yearEnd);
+    const fetchValues = async () => {
+        const newIncome = await getIncomeInRange(rangeStart, rangeEnd);
+        setIncome(newIncome / euroToMicroEuro);
+        const newExpense = await getExpenseInRange(rangeStart, rangeEnd);
+        setEarnings(newIncome - newExpense);
+        const newVatToPay = await getVATInRange(rangeStart, rangeEnd);
+        setVatToPay(newVatToPay / euroToMicroEuro);
+        const newExpenseByType = await getExpenseInRangeByType(rangeStart, rangeEnd);
+        setExpenseByType(newExpenseByType);
+    };
+
+    useEffect(() => {
+        fetchValues();
+    }, [rangeStart, rangeEnd]);
 
     return <>
+        <div className="date-range-picker">
+            <div className="date-range-picker__start">
+                <label htmlFor="date-range-picker__start">{dict.home.from}</label>
+                <input id="date-range-picker__start" type="date"
+                       defaultValue={yearBegin.toISOString().slice(0, 10)}
+                       onChange={e => setRangeStart(new Date(e.target.value))}/>
+            </div>
+            <div className="date-range-picker__end">
+                <label htmlFor="date-range-picker__end">{dict.home.to}</label>
+                <input id="date-range-picker__end" type="date"
+                       defaultValue={yearEnd.toISOString().slice(0, 10)}
+                       onChange={e => setRangeEnd(new Date(e.target.value))}/>
+            </div>
+        </div>
+
         <div className="dashboard">
             <div className="card dashboard-card">
                 <div className="dashboard-card__title">
