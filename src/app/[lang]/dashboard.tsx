b3578d1c009faae1ef3f766e83aef37f6b76f9cc
Alexander Gerstberger <a.gerstberger@web.de>
2024-04-04 12:18:56 +0200

    Fix that async was used in a client component by moving the logic into a new component

---
+++ src/app/[lang]/dashboard.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import {useEffect, useState} from "react";
+import {getIncomeInRange} from "@/db/income";
+import {euroToMicroEuro} from "@/db/schema";
+import {getExpenseInRange, getExpenseInRangeByType} from "@/db/expense";
+import {getVATInRange} from "@/db/tax";
+import {Dict} from "@/translation/dictionaries";
+
+export default function Dashboard({dict}: { dict: Dict }) {
+    const now = new Date();
+    const yearBegin = new Date(Date.UTC(now.getFullYear(), 0));
+    const yearEnd = new Date(Date.UTC(now.getFullYear(), 11, 31));
+
+    const [rangeStart, setRangeStart] = useState(yearBegin);
+    const [rangeEnd, setRangeEnd] = useState(yearEnd);
+
+    const [income, setIncome] = useState(0);
+    const [earnings, setEarnings] = useState(0);
+    const [vatToPay, setVatToPay] = useState(0);
+    const [expenseByType, setExpenseByType] = useState<Record<string, number>>({});
+
+    const fetchValues = async () => {
+        const newIncome = await getIncomeInRange(rangeStart, rangeEnd);
+        setIncome(newIncome / euroToMicroEuro);
+        const newExpense = await getExpenseInRange(rangeStart, rangeEnd);
+        setEarnings(newIncome - newExpense);
+        const newVatToPay = await getVATInRange(rangeStart, rangeEnd);
+        setVatToPay(newVatToPay / euroToMicroEuro);
+        const newExpenseByType = await getExpenseInRangeByType(rangeStart, rangeEnd);
+        setExpenseByType(newExpenseByType);
+    };
+
+    useEffect(() => {
+        fetchValues();
+    }, [rangeStart, rangeEnd]);
+
+    return <>
+        <div className="date-range-picker">
+            <div className="date-range-picker__start">
+                <label htmlFor="date-range-picker__start">{dict.home.from}</label>
+                <input id="date-range-picker__start" type="date"
+                       defaultValue={yearBegin.toISOString().slice(0, 10)}
+                       onChange={e => setRangeStart(new Date(e.target.value))}/>
+            </div>
+            <div className="date-range-picker__end">
+                <label htmlFor="date-range-picker__end">{dict.home.to}</label>
+                <input id="date-range-picker__end" type="date"
+                       defaultValue={yearEnd.toISOString().slice(0, 10)}
+                       onChange={e => setRangeEnd(new Date(e.target.value))}/>
+            </div>
+        </div>
+
+        <div className="dashboard">
+            <div className="card dashboard-card">
+                <div className="dashboard-card__title">
+                    {dict.home.income}
+                </div>
+                <div className="dashboard-card__amount">
+                    {income.toFixed(2)} €
+                </div>
+            </div>
+            <div className="card dashboard-card">
+                <div className="dashboard-card__title">
+                    {dict.home.earnings}
+                </div>
+                <div className="dashboard-card__amount">
+                    {earnings.toFixed(2)} €
+                </div>
+            </div>
+            <div className="card dashboard-card">
+                <div className="dashboard-card__title">
+                    {dict.home.vatToPay}
+                </div>
+                <div className="dashboard-card__amount">
+                    {vatToPay.toFixed(2)} €
+                </div>
+            </div>
+            {Object.keys(expenseByType).map(type =>
+                <div key={type} className="card dashboard-card">
+                    <div className="dashboard-card__title">
+                        {type}
+                    </div>
+                    <div className="dashboard-card__amount">
+                        {(expenseByType[type] / euroToMicroEuro).toFixed(2)} €
+                    </div>
+                </div>
+            )}
+        </div>
+    </>
+}
--- src/translation/dictionaries.ts
+++ src/translation/dictionaries.ts
@@ -1,8 +1,12 @@
+import {AsyncReturnType} from "@/types/asyncReturnType";
+
 export const dictionaries = {
     en: () => import('./dictionaries/en.json').then((module) => module.default),
     de: () => import('./dictionaries/de.json').then((module) => module.default)
 }
 
 export type Locales = keyof typeof dictionaries;
+
+export type Dict = AsyncReturnType<typeof dictionaries[Locales]>;
 
 export const getDictionary = async (locale: Locales) => dictionaries[locale]();
