6da236eb5a94825bb650ee7dcfedea9e71de197c
Alexander Gerstberger <a.gerstberger@web.de>
2024-04-10 16:04:36 +0200

    Refactor getVATInRange function by splitting the logic into sub-functions

--- src/db/tax.ts
+++ src/db/tax.ts
@@ -7,7 +7,15 @@ import {and, between, eq, sql, sum} from "drizzle-orm";
 
 export async function getVATInRange(start: Date, end: Date): Promise<number> {
     const income = await getIncomeInRange(start, end);
+    const taxableIncome = await getTaxableIncome(start, end, income);
 
+    const taxToBePaid = taxableIncome - (taxableIncome / 1.19);
+    const paidVat = await getPaidVatBetweenDates(start, end);
+
+    return taxToBePaid - paidVat;
+}
+
+export async function getTaxableIncome(start: Date, end: Date, income: number) {
     const differentialSum = (await db.select({
         differentialSum: sum(expense.amount).mapWith(Number),
     }).from(expense).where(
@@ -17,18 +25,17 @@ export async function getVATInRange(start: Date, end: Date): Promise<number> {
         )
     ))[0].differentialSum;
 
-    const taxableIncome = income - differentialSum;
+    return income - differentialSum;
+}
 
-    const taxToBePaid = taxableIncome - (taxableIncome / 1.19);
-
-    const paidVat = (await db.select({
+export async function getPaidVatBetweenDates(start: Date, end: Date) {
+    const result = await db.select({
         paidVat: sql`sum(${expense.amount} - (${expense.amount} / (100 + ${expense.vat}) * 100))`.mapWith(Number)
     }).from(expense).where(
         and(
             between(expense.date, start.toISOString().slice(0, 10), end.toISOString().slice(0, 10)),
             eq(expense.taxType, TaxType.VAT)
         )
-    ))[0].paidVat;
-
-    return taxToBePaid - paidVat;
+    );
+    return result[0].paidVat;
 }
