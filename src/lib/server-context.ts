1da60317e4d0a91efaab867c1e93bf6fa4d36ce0
Alexander Gerstberger <a.gerstberger@web.de>
2024-05-20 13:18:47 +0200

    Add types to getLocalizationContext

--- src/lib/server-context.ts
+++ src/lib/server-context.ts
@@ -1,7 +1,8 @@
 import {cache} from "react";
 import {getDictionary} from "@/translation/dictionaries";
+import {LocalizationContextType} from "@/lib/contexts";
 
-export const getLocalizationContext = cache(() => {
+export const getLocalizationContext = cache<() => LocalizationContextType>(() => {
     return {
         locale: "en",
         dict: getDictionary("en")
