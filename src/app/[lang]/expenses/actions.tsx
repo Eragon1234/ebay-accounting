8d64d43f67eddb374a09fbb89dc013833dea96c4
Alexander Gerstberger <a.gerstberger@web.de>
2024-06-15 12:15:46 +0200

    Add a file icon to the actions of the expense page

--- src/app/[lang]/expenses/actions.tsx
+++ src/app/[lang]/expenses/actions.tsx
@@ -3,13 +3,19 @@
 import DeleteIcon from "@/components/icons/delete-icon";
 import {Expense} from "@/db/schema";
 import {deleteExpense} from "@/db/expense";
+import FileIcon from "@/components/icons/file-icon";
+import Link from "next/link";
 
 type ActionProps = {
     expense: Expense
 }
 
 export default function Actions({expense}: ActionProps) {
-    return <div>
+    return <div style={{
+        display: "flex",
+        justifyContent: "space-between",
+    }}>
+        {expense.file && <Link href={`/files/${expense.file}`}><FileIcon/></Link>}
         <div onClick={() => deleteExpenseAction(expense)}><DeleteIcon/></div>
     </div>
 }
