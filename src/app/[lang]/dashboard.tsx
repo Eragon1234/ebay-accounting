5c8713899cbd2ff313a4d9aef15fd5332c435551
Alexander Gerstberger <a.gerstberger@web.de>
2024-07-06 19:00:19 +0200

    Turn the Dashboard into a server component

--- src/app/[lang]/dashboard.tsx
+++ src/app/[lang]/dashboard.tsx
@@ -1,53 +1,28 @@
-"use client";
-
-import {useEffect, useState} from "react";
 import {getIncomeInRange} from "@/db/income";
 import {euroToMicroEuro} from "@/db/schema";
 import {getExpenseInRange, getExpenseInRangeByType} from "@/db/expense";
 import {getVATInRange} from "@/db/tax";
 import {Dict} from "@/translation/dictionaries";
 
-export default function Dashboard({dict}: { dict: Dict }) {
-    const now = new Date();
-    const yearBegin = new Date(Date.UTC(now.getFullYear(), 0));
-    const yearEnd = new Date(Date.UTC(now.getFullYear(), 11, 31));
+type DashboardProps = {
+    dict: Dict,
+    rangeStart: Date,
+    rangeEnd: Date
+}
 
-    const [rangeStart, setRangeStart] = useState(yearBegin);
-    const [rangeEnd, setRangeEnd] = useState(yearEnd);
+export default async function Dashboard({dict, rangeStart, rangeEnd}: DashboardProps) {
+    if (isNaN(rangeStart.getTime()) || isNaN(rangeEnd.getTime())) {
+        return;
+    }
 
-    const [income, setIncome] = useState(0);
-    const [expense, setExpense] = useState(0);
-    const [vatToPay, setVatToPay] = useState(0);
-    const [expenseByType, setExpenseByType] = useState<Record<string, number>>({});
+    const income = await getIncomeInRange(rangeStart, rangeEnd);
+    const expense = await getExpenseInRange(rangeStart, rangeEnd);
+    const vatToPay = await getVATInRange(rangeStart, rangeEnd);
+    const expenseByType = await getExpenseInRangeByType(rangeStart, rangeEnd);
 
     const earnings = income - expense;
 
-    useEffect(() => {
-        if (isNaN(rangeStart.getTime()) || isNaN(rangeEnd.getTime())) {
-            return;
-        }
-        getIncomeInRange(rangeStart, rangeEnd).then(v => setIncome(v / euroToMicroEuro));
-        getExpenseInRange(rangeStart, rangeEnd).then(v => setExpense(v / euroToMicroEuro));
-        getVATInRange(rangeStart, rangeEnd).then(v => setVatToPay(v / euroToMicroEuro));
-        getExpenseInRangeByType(rangeStart, rangeEnd).then(v => setExpenseByType(v));
-    }, [rangeStart, rangeEnd]);
-
     return <>
-        <div className="date-range-picker">
-            <div className="date-range-picker__start">
-                <label htmlFor="date-range-picker__start">{dict.home.from}</label>
-                <input id="date-range-picker__start" type="date"
-                       defaultValue={yearBegin.toISOString().slice(0, 10)}
-                       onChange={e => setRangeStart(new Date(e.target.value))}/>
-            </div>
-            <div className="date-range-picker__end">
-                <label htmlFor="date-range-picker__end">{dict.home.to}</label>
-                <input id="date-range-picker__end" type="date"
-                       defaultValue={yearEnd.toISOString().slice(0, 10)}
-                       onChange={e => setRangeEnd(new Date(e.target.value))}/>
-            </div>
-        </div>
-
         <div className="dashboard">
             <div className="card dashboard-card">
                 <div className="dashboard-card__title">
--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,15 +1,42 @@
+"use client";
+
 import Dashboard from "@/app/[lang]/dashboard";
 import {getLocalizationContext} from "@/lib/server-contexts";
-import React from "react";
+import React, {useState} from "react";
 import {getDictionary, Locales} from "@/translation/dictionaries";
 
 export const dynamic = "force-dynamic";
 
-export default async function Home({params}: { params: { lang: Locales } }) {
+export default function Home({params}: { params: { lang: Locales } }) {
+    const dict = getDictionary(params.lang);
     getLocalizationContext().resolve({
         locale: params.lang,
-        dict: getDictionary(params.lang)
+        dict
     });
 
-    return <Dashboard dict={((await getLocalizationContext())).dict}/>
+    const now = new Date();
+    const yearBegin = new Date(Date.UTC(now.getFullYear(), 0));
+    const yearEnd = new Date(Date.UTC(now.getFullYear(), 11, 31));
+
+    const [rangeStart, setRangeStart] = useState(yearBegin);
+    const [rangeEnd, setRangeEnd] = useState(yearEnd);
+
+    return <>
+        <div className="date-range-picker">
+            <div className="date-range-picker__start">
+                <label htmlFor="date-range-picker__start">{dict.home.from}</label>
+                <input id="date-range-picker__start" type="date"
+                       defaultValue={yearBegin.toISOString().slice(0, 10)}
+                       onChange={e => setRangeStart(new Date(e.target.value))}/>
+            </div>
+            <div className="date-range-picker__end">
+                <label htmlFor="date-range-picker__end">{dict.home.to}</label>
+                <input id="date-range-picker__end" type="date"
+                       defaultValue={yearEnd.toISOString().slice(0, 10)}
+                       onChange={e => setRangeEnd(new Date(e.target.value))}/>
+            </div>
+        </div>
+
+        <Dashboard dict={dict} rangeStart={rangeStart} rangeEnd={rangeEnd}/>
+    </>
 }
