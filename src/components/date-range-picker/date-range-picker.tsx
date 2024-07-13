6b8d50a337cbce448f309959dfbea700d62100a9
Alexander Gerstberger <a.gerstberger@web.de>
2024-07-13 15:50:13 +0200

    Add missing use client directive in date-range-picker.tsx

--- src/components/date-range-picker/date-range-picker.tsx
+++ src/components/date-range-picker/date-range-picker.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, {useCallback} from "react";
 import {Dict} from "@/translation/dictionaries";
 import {usePathname, useRouter, useSearchParams} from "next/navigation";
