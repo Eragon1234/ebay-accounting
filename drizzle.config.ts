43956bdc484e4cde6ff17426861070c48d3d5d86
Alexander Gerstberger <a.gerstberger@web.de>
2024-08-15 18:31:15 +0200

    Upgrade dependencies

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,9 +4,5 @@ export default {
     schema: "./src/db/schema.ts",
     dialect: "sqlite",
     out: "./drizzle",
-    driver: 'd1',
-    dbCredentials: {
-        wranglerConfigPath: "",
-        dbName: ""
-    }
+    driver: 'd1-http',
 } satisfies Config;
