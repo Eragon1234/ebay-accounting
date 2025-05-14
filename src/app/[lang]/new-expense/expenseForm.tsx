5ab5dd69e6b7b86001c4c5f443acb6576f464b08
Alexander Gerstberger <a.gerstberger@web.de>
2025-05-14 12:10:43 +0200

    Remove unused import

--- src/app/[lang]/new-expense/expenseForm.tsx
+++ src/app/[lang]/new-expense/expenseForm.tsx
@@ -1,39 +1,39 @@
 "use client";
 
-import createExpenseFromForm, {getExpenseTypes} from "@/db/expense";
-import {TaxTypeInput} from "@/app/[lang]/new-expense/taxTypeInput";
-import {SubmitButton} from "@/components/submit-button/submitButton";
-import {Localization} from "@/translation/dictionaries";
+import createExpenseFromForm from "@/db/expense";
+import { TaxTypeInput } from "@/app/[lang]/new-expense/taxTypeInput";
+import { SubmitButton } from "@/components/submit-button/submitButton";
+import { Localization } from "@/translation/dictionaries";
 import { useActionState } from "react";
 
-export function ExpenseForm({localization, types}: { localization: Localization, types: string[] }) {
-    const {dict, locale} = localization;
+export function ExpenseForm({ localization, types }: { localization: Localization, types: string[] }) {
+  const { dict, locale } = localization;
 
-    const today = new Date();
-    const isoDateString = today.toISOString().slice(0, 10);
+  const today = new Date();
+  const isoDateString = today.toISOString().slice(0, 10);
 
-    const action = createExpenseFromForm.bind(null, locale);
-    const [state, formAction, pending] = useActionState(action, null);
+  const action = createExpenseFromForm.bind(null, locale);
+  const [state, formAction, pending] = useActionState(action, null);
 
-    return (
-        <form action={formAction}>
-            <label htmlFor="name">{dict.addExpense.name}</label>
-            <input type="text" id="name" name="name" required/>
-            <label htmlFor="type">{dict.addExpense.type}</label>
-            <input type="text" id="type" name="type" list="types" required/>
-            <datalist id="types">
-                {
-                    types.map(type => <option key={type} value={type}></option>)
-                }
-            </datalist>
-            <TaxTypeInput dict={dict}/>
-            <label htmlFor="amount">{dict.addExpense.amount}</label>
-            <input type="number" id="amount" name="amount" step="0.01" required/>
-            <label htmlFor="date">{dict.addExpense.date}</label>
-            <input type="date" id="date" name="date" defaultValue={isoDateString}/>
-            <label htmlFor="file">{dict.addExpense.file}</label>
-            <input type="file" name="file" id="file"/>
-            <SubmitButton text={dict.addExpense.addExpense}/>
-        </form>
-    )
+  return (
+    <form action={formAction}>
+      <label htmlFor="name">{dict.addExpense.name}</label>
+      <input type="text" id="name" name="name" required />
+      <label htmlFor="type">{dict.addExpense.type}</label>
+      <input type="text" id="type" name="type" list="types" required />
+      <datalist id="types">
+        {
+          types.map(type => <option key={type} value={type}></option>)
+        }
+      </datalist>
+      <TaxTypeInput dict={dict} />
+      <label htmlFor="amount">{dict.addExpense.amount}</label>
+      <input type="number" id="amount" name="amount" step="0.01" required />
+      <label htmlFor="date">{dict.addExpense.date}</label>
+      <input type="date" id="date" name="date" defaultValue={isoDateString} />
+      <label htmlFor="file">{dict.addExpense.file}</label>
+      <input type="file" name="file" id="file" />
+      <SubmitButton text={dict.addExpense.addExpense} />
+    </form>
+  )
 }
