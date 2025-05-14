3ca6f76b61a9049757274520f6f2a76a52e5e08e
Alexander Gerstberger <a.gerstberger@web.de>
2025-05-14 11:58:57 +0200

    Use useActionState in incomeForm.tsx

--- src/app/[lang]/new-income/incomeForm.tsx
+++ src/app/[lang]/new-income/incomeForm.tsx
@@ -1,26 +1,30 @@
-import {createIncomeFromForm} from "@/db/income";
-import {SubmitButton} from "@/components/submit-button/submitButton";
-import {Localization} from "@/translation/dictionaries";
+"use client";
 
-export async function IncomeForm({localization}: { localization: Localization }) {
-    const {dict, locale} = localization;
+import { createIncomeFromForm } from "@/db/income";
+import { SubmitButton } from "@/components/submit-button/submitButton";
+import { Localization } from "@/translation/dictionaries";
+import { useActionState } from "react";
 
-    const today = new Date();
-    const isoDateString = today.toISOString().slice(0, 10);
+export function IncomeForm({ localization }: { localization: Localization }) {
+  const { dict, locale } = localization;
 
-    const action = createIncomeFromForm.bind(null, locale);
+  const today = new Date();
+  const isoDateString = today.toISOString().slice(0, 10);
 
-    return (
-        <form action={action}>
-            <label htmlFor="name">{dict.addIncome.name}</label>
-            <input type="text" id="name" name="name" required/>
-            <label htmlFor="amount">{dict.addIncome.amount}</label>
-            <input type="number" id="amount" name="amount" step="0.01" required/>
-            <label htmlFor="date">{dict.addIncome.date}</label>
-            <input type="date" id="date" name="date" defaultValue={isoDateString} required/>
-            <label htmlFor="file">{dict.addIncome.file}</label>
-            <input type="file" name="file" id="file"/>
-            <SubmitButton text={dict.addIncome.addIncome}/>
-        </form>
-    )
+  const action = createIncomeFromForm.bind(null, locale);
+  const [state, formAction, pending] = useActionState(action, null);
+
+  return (
+    <form action={formAction}>
+      <label htmlFor="name">{dict.addIncome.name}</label>
+      <input type="text" id="name" name="name" required />
+      <label htmlFor="amount">{dict.addIncome.amount}</label>
+      <input type="number" id="amount" name="amount" step="0.01" required />
+      <label htmlFor="date">{dict.addIncome.date}</label>
+      <input type="date" id="date" name="date" defaultValue={isoDateString} required />
+      <label htmlFor="file">{dict.addIncome.file}</label>
+      <input type="file" name="file" id="file" />
+      <SubmitButton text={dict.addIncome.addIncome} />
+    </form>
+  )
 }
--- src/db/income.ts
+++ src/db/income.ts
@@ -1,72 +1,72 @@
 "use server";
 
-import {redirect} from "next/navigation";
-import {saveFile} from "@/db/files";
-import {euroToMicroEuro, Income, income, NewIncome} from "@/db/schema";
+import { redirect } from "next/navigation";
+import { saveFile } from "@/db/files";
+import { euroToMicroEuro, Income, income, NewIncome } from "@/db/schema";
 import db from "@/db/db";
-import {between, count, desc, eq, sum} from "drizzle-orm";
-import {createInsertSchema} from "drizzle-zod";
-import {revalidatePath} from "next/cache";
-import {Locales} from "@/translation/dictionaries";
+import { between, count, desc, eq, sum } from "drizzle-orm";
+import { createInsertSchema } from "drizzle-zod";
+import { revalidatePath } from "next/cache";
+import { Locales } from "@/translation/dictionaries";
 
 export async function countIncomes() {
-    return db.select({count: count(income.id)}).from(income).then(a => a[0].count);
+  return db.select({ count: count(income.id) }).from(income).then(a => a[0].count);
 }
 
 export async function getIncomes(limit: number, offset: number): Promise<Income[]> {
-    return db.query.income.findMany({
-        limit,
-        offset,
-        orderBy: [
-            desc(income.date)
-        ]
-    });
+  return db.query.income.findMany({
+    limit,
+    offset,
+    orderBy: [
+      desc(income.date)
+    ]
+  });
 }
 
 export async function getIncomeInRange(start: Date, end: Date): Promise<number> {
-    const result = await db.select({
-        sum: sum(income.amount).mapWith(Number)
-    }).from(income).where(
-        between(income.date, start.toISOString().slice(0, 10), end.toISOString().slice(0, 10))
-    );
-    return result[0].sum || 0;
+  const result = await db.select({
+    sum: sum(income.amount).mapWith(Number)
+  }).from(income).where(
+    between(income.date, start.toISOString().slice(0, 10), end.toISOString().slice(0, 10))
+  );
+  return result[0].sum || 0;
 }
 
 export async function createIncome(newIncome: NewIncome) {
-    await db.insert(income).values(newIncome)
+  await db.insert(income).values(newIncome)
 }
 
 export async function deleteIncome(id: number) {
-    await db.delete(income).where(eq(income.id, id));
-    revalidatePath("/[lang]/incomes", "page");
+  await db.delete(income).where(eq(income.id, id));
+  revalidatePath("/[lang]/incomes", "page");
 }
 
 const newIncomeSchema = createInsertSchema(income);
 
-export async function createIncomeFromForm(lang: Locales, formData: FormData) {
-    const validatedFields = newIncomeSchema.safeParse({
-        name: (formData.get("name") as string).trim(),
-        amount: Math.round(parseFloat(formData.get("amount") as string) * euroToMicroEuro),
-        date: formData.get("date"),
-    });
+export async function createIncomeFromForm(lang: Locales, prevState: any, formData: FormData) {
+  const validatedFields = newIncomeSchema.safeParse({
+    name: (formData.get("name") as string).trim(),
+    amount: Math.round(parseFloat(formData.get("amount") as string) * euroToMicroEuro),
+    date: formData.get("date"),
+  });
 
-    if (!validatedFields.success) {
-        return {
-            errors: validatedFields.error.flatten().fieldErrors
-        }
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors
     }
+  }
 
-    const file = formData.get("file") as File;
-    let path;
-    if (file && file.size > 0) {
-        path = await saveFile(file);
-    }
+  const file = formData.get("file") as File;
+  let path;
+  if (file && file.size > 0) {
+    path = await saveFile(file);
+  }
 
-    await createIncome({
-        ...validatedFields.data,
-        file: path
-    });
+  await createIncome({
+    ...validatedFields.data,
+    file: path
+  });
 
-    revalidatePath("/[lang]/incomes", "page");
-    redirect(`/${lang}/incomes`);
+  revalidatePath("/[lang]/incomes", "page");
+  redirect(`/${lang}/incomes`);
 }
