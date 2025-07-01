a515edd706ea0ffb042851c67e755654bf14653d
Alexander Gerstberger <a.gerstberger@web.de>
2025-07-01 23:16:45 +0200

    Upgrade dependencies

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,5 @@ import type {Config} from "drizzle-kit";
 
 export default {
     schema: "./src/db/schema.ts",
-    dialect: "sqlite",
     out: "./drizzle",
-    driver: 'd1-http',
 } satisfies Config;
