0a9ee4b34117a5ecb528e1a64772eb084b89ec36
Alexander Gerstberger <alexander@gerstberger.me>
2025-12-08 12:03:56 +0100

    Upgrade `recharts` and refactor `ExpenseChart` to use updated API

--- src/app/[lang]/ExpenseChart.tsx
+++ src/app/[lang]/ExpenseChart.tsx
@@ -1,21 +1,19 @@
 "use client";
 
-import {Bar, BarChart, ResponsiveContainer, Tooltip, XAxis, YAxis} from "recharts";
+import {Bar, BarChart, Tooltip, XAxis, YAxis} from "recharts";
 
 type ExpensePieProps = {
     data: { sum: number, type: string }[]
 }
 
 export default function ExpenseChart({data}: ExpensePieProps) {
-    return <ResponsiveContainer width={"100%"} minWidth={500} height={300}>
-        <BarChart height={300} data={data}>
-            <XAxis dataKey="type"/>
-            <YAxis/>
-            <Tooltip cursor={false}
-                     contentStyle={{color: "var(--foreground-color)", backgroundColor: "var(--background"}}
-                     itemStyle={{color: "var(--foreground-color)"}}
-                     separator={""} formatter={(value) => ["", value]}/>
-            <Bar dataKey="sum" fill="var(--background)" stroke={"var(--foreground-color)"}/>
-        </BarChart>
-    </ResponsiveContainer>
+    return <BarChart responsive height={300} width={"100%"} data={data}>
+        <XAxis dataKey="type"/>
+        <YAxis/>
+        <Tooltip cursor={false}
+                 contentStyle={{color: "var(--foreground-color)", backgroundColor: "var(--background"}}
+                 itemStyle={{color: "var(--foreground-color)"}}
+                 separator={""} formatter={(value) => ["", value]}/>
+        <Bar dataKey="sum" fill="var(--background)" stroke={"var(--foreground-color)"}/>
+    </BarChart>
 }
