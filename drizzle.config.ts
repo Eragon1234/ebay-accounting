3b7201b874ee776d4b960d16b41f1da2f1404c03
Alexander Gerstberger <alexander@gerstberger.me>
2026-01-23 08:11:15 +0100

    Add missing properties to drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,5 +2,7 @@ import type {Config} from "drizzle-kit";
 
 export default {
     schema: "./src/db/schema.ts",
+    dialect: "sqlite",
+    driver: "d1-http",
     out: "./drizzle",
 } satisfies Config;
