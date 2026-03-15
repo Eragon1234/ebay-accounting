944f27964f640927ffe92853184c7d5529026d0a
Alexander Gerstberger <alexander@gerstberger.me>
2026-03-15 02:09:03 +0100

    Collapse pagination links for large page counts

--- src/components/paginate/paginate.tsx
+++ src/components/paginate/paginate.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import "./paginate.css";
+import clamp from "@/lib/clamp";
+import range from "@/lib/range";
 
 type PaginateProps = {
     pageCount: number
@@ -7,29 +9,41 @@ type PaginateProps = {
 }
 
 export function Paginate({pageCount, currentPage}: PaginateProps) {
-    const pages = [];
-    for (let i = 1; i <= pageCount; i++) {
-        pages.push(i)
+    if (pageCount < 2) {
+        return null;
     }
 
-    if (pageCount <= 1) {
-        return null
-    }
+    const start = clamp(currentPage - 2, 2, pageCount);
+    const end = clamp(currentPage + 2, 2, pageCount);
 
     return (
         <div className="pagination-nav">
+            <Page page={1} isCurrent={currentPage === 1}/>
+            <Ellipsis/>
             {
-                pages.map((page) => {
-                    return <Link key={page}
-                                 href={`?page=${page}`}
-                                 className={"pagination-nav__item " +
-                                     (page === currentPage && "pagination-nav__item--active")}>
-                        {
-                            page
-                        }
-                    </Link>
-                })
+                range(start, end).map(page =>
+                    <Page
+                        key={page}
+                        page={page}
+                        isCurrent={currentPage === page}
+                    />
+                )
             }
+            <Ellipsis/>
+            <Page page={pageCount - 1} isCurrent={currentPage === pageCount - 1}/>
         </div>
     )
+}
+
+function Page({page, isCurrent}: { page: number, isCurrent: boolean }) {
+    return (
+        <Link href={`?page=${page}`}
+              className={"pagination-nav__item " + (isCurrent && "pagination-nav__item--active")}>
+            {page}
+        </Link>
+    )
+}
+
+function Ellipsis() {
+    return <span className="pagination-nav__item">...</span>
 }
