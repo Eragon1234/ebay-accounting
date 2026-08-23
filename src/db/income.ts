7695d100aeac7f555a025bc0dd32386db93ed250
Alexander Gerstberger <alexander@gerstberger.me>
2026-08-23 23:08:38 +0200

    Reformat income.ts

--- src/db/income.ts
+++ src/db/income.ts
@@ -16,62 +16,62 @@ export async function countIncomes() {
 
 export async function getIncomes(limit: number, offset: number): Promise<Income[]> {
     const db = await getDbAsync();
-  return db.query.income.findMany({
-    limit,
-    offset,
-    orderBy: [
-      desc(income.date)
-    ]
-  });
+    return db.query.income.findMany({
+        limit,
+        offset,
+        orderBy: [
+            desc(income.date)
+        ]
+    });
 }
 
 export async function getIncomeInRange(start: Date, end: Date): Promise<number> {
     const db = await getDbAsync();
-  const result = await db.select({
-    sum: sum(income.amount).mapWith(Number)
-  }).from(income).where(
-    between(income.date, start.toISOString().slice(0, 10), end.toISOString().slice(0, 10))
-  );
-  return result[0].sum || 0;
+    const result = await db.select({
+        sum: sum(income.amount).mapWith(Number)
+    }).from(income).where(
+        between(income.date, start.toISOString().slice(0, 10), end.toISOString().slice(0, 10))
+    );
+    return result[0].sum || 0;
 }
 
 export async function createIncome(newIncome: NewIncome) {
     const db = await getDbAsync();
-  await db.insert(income).values(newIncome)
+    await db.insert(income).values(newIncome)
 }
 
 export async function deleteIncome(id: number) {
     const db = await getDbAsync();
-  await db.delete(income).where(eq(income.id, id));
-  revalidatePath("/[lang]/incomes", "page");
+    await db.delete(income).where(eq(income.id, id));
+    revalidatePath("/[lang]/incomes", "page");
 }
 
 const newIncomeSchema = createInsertSchema(income);
 
 export async function createIncomeFromForm(lang: Locales, prevState: any, formData: FormData) {
-  const validatedFields = newIncomeSchema.safeParse({
-    name: (formData.get("name") as string).trim(),
-    amount: Math.round(parseFloat(formData.get("amount") as string) * euroToMicroEuro),
-    date: formData.get("date"),
-  });
+    const validatedFields = newIncomeSchema.safeParse({
+        name: (formData.get("name") as string).trim(),
+        amount: Math.round(parseFloat(formData.get("amount") as string) * euroToMicroEuro),
+        date: formData.get("date"),
+    });
 
-  if (!validatedFields.success) {
-    return {
-      errors: validatedFields.error.flatten().fieldErrors
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors
+        }
     }
-  }
 
-  const file = formData.get("file") as File;
-  let path;
-  if (file && file.size > 0) {
-    path = await saveFile(file);
-  }
+    const file = formData.get("file") as File;
+    let path;
+    if (file && file.size > 0) {
+        path = await saveFile(file);
+    }
 
-  await createIncome({
-    ...validatedFields.data,
-    file: path
-  });
+    await createIncome({
+        ...validatedFields.data,
+        file: path
+    });
 
-  revalidatePath("/[lang]/incomes", "page");
-  redirect(`/${lang}/incomes`);
+    revalidatePath("/[lang]/incomes", "page");
+    redirect(`/${lang}/incomes`);
 }
